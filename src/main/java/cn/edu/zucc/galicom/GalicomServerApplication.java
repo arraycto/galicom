9ff60933c8b2b14dc2ce681cd16633576e289ce4
DESKTOP-7D73BLM\胡奕舟 <739820174@qq.com>
2021-02-02 18:46:01 +0800

    初始化后端架构

---
+++ src/main/java/cn/edu/zucc/galicom/GalicomServerApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.zucc.galicom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalicomServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GalicomServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/edu/zucc/galicom/GalicomServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.zucc.galicom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalicomServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
