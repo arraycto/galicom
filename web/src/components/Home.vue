f9740294b513a43eb0f5be5a9309bd06b65e5e90
胡奕舟 <739820174@qq.com>
2021-02-07 21:54:13 +0800

    logo新增隐藏

--- web/src/components/Home.vue
+++ web/src/components/Home.vue
@@ -1,33 +1,79 @@
 <template>
   <a-layout id="components-layout-demo-custom-trigger">
-    <a-layout-sider style="background:white" v-model="collapsed" :trigger="null" collapsible>
-      <a-avatar class="logo" src="http://www.icosky.com/icon/ico/Game/Super%20Mario/Retro%20Mario.ico"/>
-        <div class="font4">
-          <p><i>gali</i></p>
+    <a-layout-sider
+      style="background: white"
+      v-model="collapsed"
+      :trigger="null"
+      collapsible
+    >
+      <div class="logo colLayout">
+        <a-avatar
+          src="http://www.icosky.com/icon/ico/Game/Super%20Mario/Retro%20Mario.ico"
+        />
+        <div
+          class="font4"
+          style="margin-left: 25px; margin-top: -8px"
+          v-show="!collapsed"
+        >
+          <p><i>galiview</i></p>
+        </div>
       </div>
+
       <a-menu mode="inline" :default-selected-keys="['1']">
         <a-menu-item key="1">
           <a-icon type="user" />
-          <span>nav 1</span>
+          <span>首页</span>
         </a-menu-item>
         <a-menu-item key="2">
           <a-icon type="video-camera" />
-          <span>nav 2</span>
+          <span>游戏</span>
         </a-menu-item>
         <a-menu-item key="3">
           <a-icon type="upload" />
-          <span>nav 3</span>
+          <span>动漫</span>
+        </a-menu-item>
+        <a-menu-item>
+          <div
+            style="
+              border-bottom: 3px solid rgba(255, 240, 245);
+              margin-top: 10px;
+            "
+          ></div>
         </a-menu-item>
       </a-menu>
     </a-layout-sider>
     <a-layout>
-      <a-layout-header style="display:flex; flex:row; background: #fff; padding: 0">
-        <a-icon
-          class="trigger"
-          :type="collapsed ? 'menu-unfold' : 'menu-fold'"
-          @click="() => (collapsed = !collapsed)" 
-        />
-        <a-input-search style="margin-top: 15px; width: 500px; margin-left:450px" placeholder="input search text" enter-button @search="onSearch" />
+      <a-layout-header style="background: #fff; padding: 0">
+        <div style="position: relative">
+          <a-icon
+            class="trigger"
+            :type="collapsed ? 'menu-unfold' : 'menu-fold'"
+            @click="() => (collapsed = !collapsed)"
+          />
+          <a-input-search
+            style="
+              width: 350px;
+              position: absolute;
+              left: 45%;
+              top: 50%;
+              transform: translate(-50%, -50%);
+            "
+            placeholder="你要搜索的内容"
+            enter-button
+            @search="onSearch"
+          />
+          <a-avatar
+            shape="square"
+            icon="user"
+            style="
+              position: absolute;
+              right: 0;
+              right: 0;
+              transform: translateY(50%);
+              margin-right: 50px;
+            "
+          />
+        </div>
       </a-layout-header>
       <a-layout-content
         :style="{ margin: '24px 16px', padding: '24px', minHeight: '1000px' }"
@@ -76,8 +122,13 @@ export default {
 }
 
 #components-layout-demo-custom-trigger .font4 > p {
-  font-size: 40px;
+  font-size: 30px;
   text-shadow: 5px 5px 5px white, 0px 0px 2px black, 2px 2px 3px white;
 }
 
+.colLayout {
+  display: flex;
+  flex-direction: row;
+  justify-content: left;
+}
 </style>
